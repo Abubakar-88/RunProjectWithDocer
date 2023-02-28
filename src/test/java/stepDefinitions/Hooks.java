e51bac37fce1260ca71463344a0cdabddca9c9ff
Abu Bakar <siddique0153@gmail.com>
2023-02-28 10:12:32 +0600

    I have changed

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -14,6 +14,7 @@ public class Hooks extends Base{
 		ChromeOptions option=new ChromeOptions();
 		//option.setHeadless(true);
 		WebDriverManager.chromedriver().setup();
+		
 		driver=new ChromeDriver(option);
 		
 		
