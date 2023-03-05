61f1d44ca0708a1cd8e99a97cc91fbd1067e99d8
Abu Bakar <siddique0153@gmail.com>
2023-03-05 08:50:50 +0600

    hooks changed

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -11,11 +11,24 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Hooks extends Base{
 	@Before
 	public void setup() {
-		ChromeOptions option=new ChromeOptions();
-		option.setHeadless(true);
+		ChromeOptions options=new ChromeOptions();
+		options.setHeadless(true);
+		options.addArguments("--headless");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--allow-insecure-localhost");
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("start-maximized");
+		options.addArguments("enable-automation");
+		options.addArguments("--disable-infobars");
+		options.addArguments("--disable-browser-side-navigation");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--force-device-scale-factor=1");
+		
+		
 		WebDriverManager.chromedriver().setup();
 		
-		driver=new ChromeDriver(option);
+		driver=new ChromeDriver(options);
 		
 		
 	}
