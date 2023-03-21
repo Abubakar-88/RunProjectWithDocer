831aea02b2884481e8ed8cb93fc4a5d703b7ad2a
Abu Bakar <siddique0153@gmail.com>
2023-03-21 12:04:20 +0600

    udpate hook

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -19,36 +19,51 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Hooks extends Base{
 	@Before
 	public void setup() {
+		 WebDriver driver = new ChromeDriver();
 		try {
 		//	ChromeOptions option = new ChromeOptions();
 		//WebDriverManager.chromedriver().setup();
 		//driver = new ChromeDriver(option);
 		//option.addArguments("--headless");
 		//option.addArguments("--no-sandbox");
-	//	
+	// driver.get("https://www.example.com");	
 	//	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		//JENKIN
 		//DesiredCapabilities capabilities = new DesiredCapabilities();
 	//	driver = new RemoteWebDriver(capabilities);
 	//	Capabilities actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
 		//driver.get("https://www.google.com/");
-		
+			 driver.get("https://it.microtechlimited.com/");
 		
             // Set the path to the chromedriver executable
-           System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        //   System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
             // Launch Chrome browser
-           WebDriver driver = new ChromeDriver();
+          
 
             // Navigate to a URL
-            driver.get("https://it.microtechlimited.com/");
+           // driver.get("");
 
             // Close the browser
            // driver.quit();
         } catch (WebDriverException e) {
             // Handle the exception
-            System.out.println("Chrome browser failed to start: " + e.getMessage());
-            e.printStackTrace();
+//            System.out.println("Chrome browser failed to start: " + e.getMessage());
+//            e.printStackTrace();
+            if (e.getMessage().contains("chrome location /usr/bin/google-chrome")) {
+                System.out.println("ChromeDriver is assuming that Chrome has crashed.");
+                // Restart ChromeDriver and try again
+                driver.quit();
+                driver = new ChromeDriver();
+                driver.get("https://www.example.com");
+            } else {
+                // Handle other types of exceptions
+                System.out.println("WebDriverException occurred: " + e.getMessage());
+            
+            
+            
+            }
+            
         }
 		
 		
@@ -65,7 +80,7 @@ public class Hooks extends Base{
 	}	
 	@After
 	public void tearDown() {
-		driver.close();
+		 driver.quit();
 	}
 
 }
