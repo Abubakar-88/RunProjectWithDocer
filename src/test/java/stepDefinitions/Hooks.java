aac1c0c120115386c8f4def8ab4d5cfe5cd81da8
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-04-05 16:45:23 +0600

    Update Hooks.java

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -26,9 +26,9 @@ public class Hooks extends Base{
 	     ChromeOptions options = new ChromeOptions();
 		 options.addArguments("--remote-allow-any-origin");
 	      options.addArguments("--start-maximized");
-		options.addArguments('--disable-dev-shm-usage')
+		options.addArguments('--disable-dev-shm-usage');
 			option.addArguments("--no-sandbox");
-		//option.addArguments("--headless");
+		option.addArguments("--headless");
 	   // Initialize the ChromeDriver with options
 	      WebDriver driver = new ChromeDriver(options);
 		DesiredCapabilities capabilities = new DesiredCapabilities();
