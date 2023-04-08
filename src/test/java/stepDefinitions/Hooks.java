3f22a6791b662dcad8bb613b53bccfef463930fa
Muhammad Fawad <fawadqureshi1995@gmail.com>
2023-04-08 18:16:50 +0500

    chrome fix

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -22,6 +22,7 @@ public class Hooks {
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-debugging-port=9222");
 		options.addArguments("--remote-debugging-address=0.0.0.0");
 
