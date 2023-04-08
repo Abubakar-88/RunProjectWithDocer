8da0d8a0e653f7438b9b511131d42fa8e603b3ff
Muhammad Fawad <fawadqureshi1995@gmail.com>
2023-04-08 17:06:51 +0500

    chrome fix

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,15 +1,35 @@
-from selenium import webdriver
+package stepDefinitions;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-chromeOptions = webdriver.ChromeOptions()
-chromeOptions.add_argument("--no-sandbox")
-chromeOptions.add_argument("--disable-setuid-sandbox")
-chromeOptions.add_argument("--remote-debugging-port=9222")
-chromeOptions.add_argument("--disable-dev-shm-using")
-chromeOptions.add_argument("--disable-extensions")
-chromeOptions.add_argument("--disable-gpu")
-chromeOptions.add_argument("start-maximized")
-chromeOptions.add_argument("disable-infobars")
-
-b = webdriver.Chrome(chrome_options=chromeOptions)
-b.get("https://google.com/")
-b.quit()
+public class Hooks {
+	private WebDriver driver;
+	
+	@Before
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-setuid-sandbox"); 
+		options.addArguments("--remote-debugging-port=9222");
+		options.addArguments("--disable-dev-shm-using"); 
+		options.addArguments("--disable-extensions"); 
+		options.addArguments("--disable-gpu");
+		options.addArguments("--start-maximized");
+		options.addArguments("--disable-infobars");
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@After
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit(); 
+		}
+	}
+}
