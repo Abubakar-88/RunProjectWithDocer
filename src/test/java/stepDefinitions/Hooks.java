b70e3b9761f86db7b8e2a96defad0e0afdba80a2
Muhammad Fawad <fawadqureshi1995@gmail.com>
2023-04-08 17:14:38 +0500

    chrome fix

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,35 +1,47 @@
 package stepDefinitions;
+
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.ScreenshotException;
+
+import base.Base;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Hooks {
-	private WebDriver driver;
-	
+public class Hooks extends Base{
 	@Before
 	public void setup() {
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-setuid-sandbox"); 
-		options.addArguments("--remote-debugging-port=9222");
-		options.addArguments("--disable-dev-shm-using"); 
-		options.addArguments("--disable-extensions"); 
-		options.addArguments("--disable-gpu");
-		options.addArguments("--start-maximized");
-		options.addArguments("--disable-infobars");
-		driver = new ChromeDriver(options);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+	     ChromeOptions options = new ChromeOptions();
+		 options.setBinary("/usr/bin/google-chrome");     
+		 options.addArguments("--remote-allow-any-origin");
+	     options.addArguments("--start-maximized");
+		option.addArguments("--headless");
+		option.addArguments("--disable-gpu");
+		option.addArguments("--remote-debugging-port=9222");
+	   // Initialize the ChromeDriver with options
+	      WebDriver driver = new ChromeDriver(options);
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		driver = new RemoteWebDriver(capabilities);
+		
+		
 	}
-	
 	@After
 	public void tearDown() {
-		if (driver != null) {
-			driver.quit(); 
-		}
+		  if (driver != null) {
+                  driver.quit(); 
+                 }
 	}
+
 }
