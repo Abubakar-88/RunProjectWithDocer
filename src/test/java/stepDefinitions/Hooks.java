27fe0546f829ec29b9b693b3ff1cc3deaacad798
Muhammad Fawad <fawadqureshi1995@gmail.com>
2023-04-08 17:03:10 +0500

    chrome fix

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,40 +1,12 @@
-package stepDefinitions;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class Hooks {
-
-    private WebDriver driver;
-
-    @Before
-    public void setup() {
-        ChromeOptions options = new ChromeOptions()
-            .setBinary("/usr/bin/google-chrome")
-            .addArguments("--remote-allow-any-origin", "--start-maximized", "--headless", "--disable-gpu", "--disable-dev-shm-usage", "--no-sandbox");
-
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
-    @After
-    public void tearDown() {
-        try {
-            if (driver != null) {
-                driver.quit(); 
-            }
-        } catch (WebDriverException e) {
-            System.err.println("Exception during WebDriver teardown: " + e.getMessage());
-        }
-    }
-}
+chromeOptions = webdriver.ChromeOptions();
+chromeOptions.add_argument("--no-sandbox");
+chromeOptions.add_argument("--disable-setuid-sandbox"); 
+chromeOptions.add_argument("--remote-debugging-port=9222");
+chromeOptions.add_argument("--disable-dev-shm-using"); 
+chromeOptions.add_argument("--disable-extensions"); 
+chromeOptions.add_argument("--disable-gpu");
+chromeOptions.add_argument("start-maximized");
+chromeOptions.add_argument("disable-infobars");
+b = webdriver.Chrome(chrome_options=chromeOptions) 
+b.get("https://google.com/") ;
+b.quit();
