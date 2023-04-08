9498e9eebd52e59802e39952940f8413e8a1f5da
Muhammad Fawad <fawadqureshi1995@gmail.com>
2023-04-09 00:34:03 +0500

    chrome fix

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -12,30 +12,28 @@ import io.cucumber.java.Before;
 public class Hooks {
     private WebDriver driver;
 
+    /**
+     * 
+     */
     @Before
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.setBinary(" sudo /usr/bin/google-chrome");     
-        options.addArguments("--remote-allow-any-origin");
-        options.addArguments("--start-maximized");
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-debugging-port=9222");
-		options.addArguments("--remote-debugging-address=0.0.0.0");
-
-        // Initialize the ChromeDriver with options
-        driver = new ChromeDriver(options);
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        driver = new RemoteWebDriver(capabilities);
-    }
-
-    // @After
-    // public void tearDown() {
-    //     if (driver != null) {
-    //         driver.quit(); 
-    //     }
-    // }
+public void setup() {
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-any-origin");
+    options.addArguments("--start-maximized");
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--remote-debugging-port=9222");
+    options.addArguments("--remote-debugging-address=0.0.0.0");
+    driver = new ChromeDriver(options);
 }
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit(); 
+        }
+    }
+}
+
