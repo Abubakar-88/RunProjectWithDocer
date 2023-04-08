71ff80363a84ce1ac9f683aeed795b0a89899ab9
Muhammad Fawad <fawadqureshi1995@gmail.com>
2023-04-08 16:58:56 +0500

    chrome fix

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,40 +1,40 @@
 package stepDefinitions;
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.ScreenshotException;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hooks {
-	private WebDriver driver;
-	
-	@Before
-	public void setup() {
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
-		options.setBinary("/usr/bin/google-chrome");     
-		options.addArguments("--remote-allow-any-origin");
-		options.addArguments("--start-maximized");
-		options.addArguments("--headless");
-		options.addArguments("--disable-gpu");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--no-sandbox");
-		driver = new ChromeDriver(options);
-		driver.get("https://www.google.com");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
-	
-	@After
-	public void tearDown() {
-		if (driver != null) {
-			driver.quit(); 
-		}
-	}
 
+    private WebDriver driver;
+
+    @Before
+    public void setup() {
+        ChromeOptions options = new ChromeOptions()
+            .setBinary("/usr/bin/google-chrome")
+            .addArguments("--remote-allow-any-origin", "--start-maximized", "--headless", "--disable-gpu", "--disable-dev-shm-usage", "--no-sandbox");
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            if (driver != null) {
+                driver.quit(); 
+            }
+        } catch (WebDriverException e) {
+            System.err.println("Exception during WebDriver teardown: " + e.getMessage());
+        }
+    }
 }
