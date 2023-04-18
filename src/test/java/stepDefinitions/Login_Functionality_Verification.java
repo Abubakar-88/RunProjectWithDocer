565ffcd5fad9e51c24d33d33c3511a5f6f6f7b80
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-04-18 11:25:17 +0600

    Update Login_Functionality_Verification.java

--- src/test/java/stepDefinitions/Login_Functionality_Verification.java
+++ src/test/java/stepDefinitions/Login_Functionality_Verification.java
@@ -19,44 +19,44 @@ public class Login_Functionality_Verification extends Base{
 		assertEquals("Welcome to MicroTech NA.", s);
 	}
 
-	@When("I Click on Login")
-	public void i_Click_on_Login() {
-		click(By.xpath("//a[@href=\"elogin.php\"]"));
-	}
+// 	@When("I Click on Login")
+// 	public void i_Click_on_Login() {
+// 		click(By.xpath("//a[@href=\"elogin.php\"]"));
+// 	}
 
-	@Then("I will be in Login Page")
-	public void i_will_be_in_Login_Page() {
-		String s = getText(By.xpath("//div//h1"));
-		assertEquals("Employee Login", s);
-	}
+// 	@Then("I will be in Login Page")
+// 	public void i_will_be_in_Login_Page() {
+// 		String s = getText(By.xpath("//div//h1"));
+// 		assertEquals("Employee Login", s);
+// 	}
 	
-	@Then("Click Customer Login")
-	public void click_Customer_Login() {
-		click(By.xpath("//a[@href=\"clogin.php\"]"));
-		String customerLogin= getText(By.xpath("//div/h1"));
-		assertEquals("Customer Login", customerLogin);		
-	}
+// 	@Then("Click Customer Login")
+// 	public void click_Customer_Login() {
+// 		click(By.xpath("//a[@href=\"clogin.php\"]"));
+// 		String customerLogin= getText(By.xpath("//div/h1"));
+// 		assertEquals("Customer Login", customerLogin);		
+// 	}
 
-	@Then("I enter User Id {string}")
-	public void i_enter_User_Id(String string) {
-		sendKeys(By.name("mailuid"),string);
-	}
+// 	@Then("I enter User Id {string}")
+// 	public void i_enter_User_Id(String string) {
+// 		sendKeys(By.name("mailuid"),string);
+// 	}
 
-	@Then("I enter Password {string}")
-	public void i_enter_Password(String string) {
-		sendKeys(By.name("pwd"),string);
-	}
+// 	@Then("I enter Password {string}")
+// 	public void i_enter_Password(String string) {
+// 		sendKeys(By.name("pwd"),string);
+// 	}
 	
-	@When("I click Login Button")
-	public void i_click_Login_Button() {
-	    click(By.xpath("//input[@name=\"login-submit\"]"));
-	}
+// 	@When("I click Login Button")
+// 	public void i_click_Login_Button() {
+// 	    click(By.xpath("//input[@name=\"login-submit\"]"));
+// 	}
 	
-	@Then("I will see Customer Home Page")
-	public void i_will_see_Customer_Home_Page() {
-	    String s= getText(By.xpath("//h2[text()='Welcome David']"));
-	    assertEquals("Welcome David",s);
-	    System.out.println(s);
-	}
+// 	@Then("I will see Customer Home Page")
+// 	public void i_will_see_Customer_Home_Page() {
+// 	    String s= getText(By.xpath("//h2[text()='Welcome David']"));
+// 	    assertEquals("Welcome David",s);
+// 	    System.out.println(s);
+// 	}
 
 }
