a96924359a94e517c46b0dc7630d931fbfa56899
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-04-18 10:59:17 +0600

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -10,7 +10,8 @@ import io.cucumber.junit.CucumberOptions;
 		features = {"src/test/resources/features"}, 
 		glue = {"stepDefinitions"}, 
 	        tags = "@smoke",
-               plugin = {"pretty", "html:test-output", "json:target/cucumber-report/cucumber.json" }
+              //  {"pretty", "html:test-output", "json:target/cucumber-report/cucumber.json" }
+	      plugin =  {"pretty", "html:target/cucumber-reports"}
 )
 public class Runner {
 
