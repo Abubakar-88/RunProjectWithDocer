87f257fb8aaee4ad83f1ac48cfdc00a7454389b1
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-04-19 21:39:12 +0600

    Update Hooks.java

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -13,26 +13,13 @@ import io.cucumber.java.Before;
 public class Hooks {
     private WebDriver driver;
 
-    /**
-     * 
-     */
     @Before
 public void setup() throws MalformedURLException, InterruptedException {
-//     System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-//     ChromeOptions options = new ChromeOptions();
-//     options.addArguments("--remote-allow-any-origin");
     ChromeOptions opt = new ChromeOptions();
 		
 	 driver = new RemoteWebDriver(new URL("http://3.145.197.131:4444"),opt);
     
-//     options.addArguments("--start-maximized");
-//     options.addArguments("--headless");
-//     options.addArguments("--no-sandbox");
-//     options.addArguments("--disable-gpu");
-//     options.addArguments("--disable-dev-shm-usage");
-//     options.addArguments("--remote-debugging-port=9222");
-//     options.addArguments("--remote-debugging-address=0.0.0.0");
-  //  driver = new ChromeDriver(options);
+
 }
     @After
     public void tearDown() {
