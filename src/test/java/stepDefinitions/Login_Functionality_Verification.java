fa7dd48397019b84c4d9181d805658e919333a5c
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-04-19 22:12:55 +0600

    Update Login_Functionality_Verification.java

--- src/test/java/stepDefinitions/Login_Functionality_Verification.java
+++ src/test/java/stepDefinitions/Login_Functionality_Verification.java
@@ -10,7 +10,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class Login_Functionality_Verification {
+public class Login_Functionality_Verification extends Base {
 	  private WebDriver driver;
 
     @Before
