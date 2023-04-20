7cfebea874ac511bbad6f591060a8b6ebcd29a82
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-04-20 10:39:20 +0600

    Update Hooks.java

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -15,7 +15,7 @@ public class Hooks extends Base {
     private WebDriver driver;
 
     @Before
-public void setup() throws MalformedURLException, InterruptedException {
+public void setup() throws MalformedURLException {
     ChromeOptions opt = new ChromeOptions();
 		
 	 driver = new RemoteWebDriver(new URL("http://3.145.197.131:4444"),opt);
